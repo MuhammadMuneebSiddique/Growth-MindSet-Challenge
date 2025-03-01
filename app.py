3f346d72c43ac196bfc211da3b720817a8ce74ca
Muhammad Muneeb Siddique <siddiquimuneeb894@gmail.com>
2025-02-28 22:20:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import os
 from io import BytesIO
-import fitz
 
 # Configure the app
 st.set_page_config(page_title="Data Sweeper", layout="wide", initial_sidebar_state="expanded")
